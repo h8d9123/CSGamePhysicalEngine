e575ee48a2d79abdb8d5cff323a928c5514c7774
kirito <2466621355@qq.com>
2019-05-04 02:10:05 +0800

    ignore libs, modify makesetup.s

--- makesetup.sh
+++ makesetup.sh
@@ -8,4 +8,27 @@ export PROJECT_BIN=${PROJECT_DIR}/bin
 export PROJECT_LIBS=${PROJECT_DIR}/libs
 export PROJECT_TEST=${PROJECT_DIR}/test 
 export PROJECT_3RD=${PROJECT_DIR}/3rd
-export PKG_CONFIG_PATH=/usr/lib/pkgconfig:/usr/local/lib/pkgconfig
+
+#update pkg-config
+source $PROJECT_3RD/configs/update-pkg-config.sh
+if [ $PKG_CONFIG_PATH ];then
+    export PKG_CONFIG_PATH=$PROJECT_3RD/configs:$PKG_CONFIG_PATH
+else
+    export PKG_CONFIG_PATH=$PROJECT_3RD/configs
+fi
+
+if [ $LD_LIBRARY_PATH ];then
+    export LD_LIBRARY_PATH=$PROJECT_LIBS:$LD_LIBRARY_PATH
+else
+    export LD_LIBRARY_PATH=$PROJECT_LIBS
+fi
+
+#add include-dir for g++
+#update CPLUS_INCLUDE_PATH
+source $PROJECT_3RD/configs/update-cflags.sh
+export CPLUS_INCLUDE_PATH
+
+#dump env
+echo "PROJECT_DIR=$PROJECT_DIR"
+echo "LD_LIBRARY_PATH=$LD_LIBRARY_PATH"
+echo "CPLUS_INCLUDE_PATH=$CPLUS_INCLUDE_PATH"
