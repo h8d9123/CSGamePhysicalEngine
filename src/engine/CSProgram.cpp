5c0a39fbf7e3336b011fb3018d1e62a1a409015c
wanghz <h8d9123@163.com>
2019-06-09 16:27:15 +0800

    wrap glProgram as class GLProgram

---
+++ src/engine/CSProgram.cpp
@@ -0,0 +1,303 @@
+#include "CSProgram.h"
+/**
+ *@brief constructor.
+ */
+GLProgram::GLProgram()
+{
+    m_id = glCreateProgram();
+}
+
+
+/**
+ *@brief Attach a shader to program
+ *@param shaderId Specifies a shader that's to be attached.
+ */
+void GLProgram::attach(GLuint shaderId)
+{
+    glAttachShader(m_id, shaderId);
+}
+
+/**
+ *@brief Attach a shader to program
+ *@param shaderId Specifies a shader that's to be attached.
+ */
+void GLProgram::detach(GLuint shaderId)
+{
+    glDetachShader(m_id, shaderId);
+}
+
+/**
+ *@return true if program is currently flagged for deletion, otherwise false
+ */
+bool
+GLProgram::isDeleted()
+{
+    GLint params;
+    glGetProgramiv(m_id, GL_DELETE_STATUS, &params);
+    return params;
+}
+
+/**
+ *@return true if program is linked successfully, otherwise false
+ */
+bool
+GLProgram::isLinked()
+{
+    GLint params;
+    glGetProgramiv(m_id, GL_LINK_STATUS, &params);
+    return params;
+}
+
+
+/**
+ *@return true if program is validate successfully, otherwise false
+ */
+bool
+GLProgram::isValidate()
+{
+    GLint params;
+    glGetProgramiv(m_id, GL_VALIDATE_STATUS, &params);
+    return params;
+}
+
+/**
+ *@return the number of the log about program 
+ */
+GLint
+GLProgram::getLogLength()
+{
+    GLint params;
+    glGetProgramiv(m_id, GL_INFO_LOG_LENGTH, &params);
+    return params;
+}
+
+/**
+ *@return the number of shaders attached to program 
+ */
+GLint
+GLProgram::getAttachedShadersNumber()
+{
+    GLint params;
+    glGetProgramiv(m_id, GL_ATTACHED_SHADERS, &params);
+    return params;
+}
+
+/**
+ *@return the number of active attribute atomic counter buffers used by program 
+ */
+GLint
+GLProgram::getActiveAtomicCounterBuffers()
+{
+    GLint params;
+    glGetProgramiv(m_id, GL_ACTIVE_ATOMIC_COUNTER_BUFFERS, &params);
+    return params;
+}
+
+/**
+ *@return the length of the longest active attribute name for program​,
+ */
+GLint
+GLProgram::getActiveAttributesMaxLength()
+{
+    GLint params;
+    glGetProgramiv(m_id, GL_ACTIVE_ATTRIBUTE_MAX_LENGTH, &params);
+    return params;
+}
+
+/**
+ *@return the number of active attribute variables for program​.
+ */
+GLint
+GLProgram::getActiveAttributesNumber()
+{
+    GLint params;
+    glGetProgramiv(m_id, GL_ACTIVE_ATTRIBUTES, &params);
+    return params;
+}
+
+/**
+ *@return the number of active uniform variables for program​.
+ */
+GLint
+GLProgram::getActiveUniformNumber()
+{
+    GLint params;
+    glGetProgramiv(m_id, GL_ACTIVE_UNIFORMS, &params);
+    return params;
+}
+
+/**
+ *@return the length of the longest active uniform variable name for program​
+ */
+GLint
+GLProgram::getActiveUniformMaxLength()
+{
+    GLint params;
+    glGetProgramiv(m_id, GL_ACTIVE_UNIFORM_MAX_LENGTH, &params);
+    return params;
+}
+
+/**
+ *@return  the length of the program binary
+ */
+GLint
+GLProgram::getProgramBinaryLength()
+{
+    GLint params;
+    glGetProgramiv(m_id, GL_PROGRAM_BINARY_LENGTH, &params);
+    return params;
+}
+
+/**
+ *@brief  get the local work group size of the compute program as specified by its input layout qualifier
+ *@params [output] an array of three integers.
+ *@note gl>=4.3
+ */
+void
+GLProgram::getComputeWorkGroupSize(GLint* params)
+{
+    glGetProgramiv(m_id, GL_COMPUTE_WORK_GROUP_SIZE, params);
+}
+
+/**
+ *@return  a symbolic constant indicating the buffer mode used when transform feedback is active. This may be GL_SEPARATE_ATTRIBS or GL_INTERLEAVED_ATTRIBS.
+ */
+GLint
+GLProgram::getTransformFeedbackBufferMode()
+{
+    GLint params;
+    glGetProgramiv(m_id, GL_TRANSFORM_FEEDBACK_BUFFER_MODE, &params);
+    return params;
+}
+
+/**
+ *@return  returns the number of varying variables to capture in transform feedback mode for the program.
+ */
+GLint
+GLProgram::getTransformFeedbackVaryingVariableNumber()
+{
+    GLint params;
+    glGetProgramiv(m_id, GL_TRANSFORM_FEEDBACK_VARYINGS, &params);
+    return params;
+}
+
+/**
+ *@return the length of the longest variable name to be used for transform feedback
+ */
+GLint
+GLProgram::getTransformFeedbackVaryingVariableMaxLength()
+{
+    GLint params;
+    glGetProgramiv(m_id, GL_TRANSFORM_FEEDBACK_VARYING_MAX_LENGTH, &params);
+    return params;
+}
+
+/**
+ *@return returns the maximum number of vertices that the geometry shader in program​ will output
+ *@note gl>=3.2
+ */
+GLint
+GLProgram::getGeometryVerticesOutputNumber()
+{
+    GLint params;
+    glGetProgramiv(m_id, GL_GEOMETRY_VERTICES_OUT, &params);
+    return params;
+}
+
+/**
+ *@return a symbolic constant indicating the primitive type accepted as input to the geometry shader contained in program​.
+ *@note gl>=3.2
+ */
+GLint
+GLProgram::getGeometryInputNumber()
+{
+    GLint params;
+    glGetProgramiv(m_id, GL_GEOMETRY_INPUT_TYPE, &params);
+    return params;
+}
+
+/**
+ *@return a symbolic constant indicating the primitive type that will be output by the geometry shader contained in program​.
+ *@note gl>=3.2
+ */
+GLint
+GLProgram::getGeometryOutputNumber()
+{
+    GLint params;
+    glGetProgramiv(m_id, GL_GEOMETRY_OUTPUT_TYPE, &params);
+    return params;
+}
+
+/**
+ *@briefreturns the information log for the specified program object. The information log for a program object is modified when the program object is linked or validated. The string that is returned will be null terminated. 
+ *@param log specifies string that is used to return detail infomation.
+ */
+void
+GLProgram::getLog(std::string &log)
+{
+    if (!log.empty()) log.clear();
+    char msg[1024];
+    int len= 0;
+    glGetProgramInfoLog(m_id, 1024, &len, msg);
+    log = msg;
+}
+
+/*
+ *@return true if program​ is the name of a program object previously created with glCreateProgram and not yet deleted with glDeleteProgram. If program​ is zero or a non-zero value that is not the name of a program object, or if an error occurs, glIsProgram returns false.
+ */
+bool
+GLProgram::isProgram()
+{
+    return glIsProgram(m_id);
+}
+
+/**
+ *@brief get shaders attached to program
+ *@param maxCnt the max number of shader to be queried
+ *@param count the queried shader number
+ *@param shaders an array contains queried shaders id.
+ *@note getAttatchedShadersNumber() can get shader number.
+ */
+void
+GLProgram::getAttachedShaders(GLsizei maxCnt, GLsizei &count, GLuint* shaders)
+{
+    assert(shaders);
+    glGetAttachedShaders(m_id, maxCnt, &count, shaders);
+}
+
+/**
+ *@return true if program is linked successfully, otherwise false.
+ *@see glLinkprogram()
+ */
+bool
+GLProgram::link()
+{
+    glLinkProgram(m_id);
+    return isLinked();
+}
+/**
+ *@brief use this program.
+ */
+void
+GLProgram::use()
+{
+    glUseProgram(m_id);
+}
+
+/**
+ *@brief set gl program
+ */
+void
+GLProgram::setProgramId(GLuint programId)
+{
+    m_id = programId;
+}
+
+/**
+ *@brief get gl program
+ */
+GLuint
+GLProgram::getProgramId()
+{
+    return m_id;
+}
---
+++ src/engine/CSProgram.h
@@ -0,0 +1,62 @@
+#pragma once 
+/**
+ * @file CSProgram.h
+ * @brief
+ * @author wanghzh
+ * @version 1.0
+ * @date 2019-6-9 
+ */
+
+
+/**
+ *@class GLProgram
+ *@brief wraps glprogram function
+ *@author wanghzh
+ *@version 1.0
+ *@date 2019-06-09
+ */
+
+#include <GL/glew.h>
+#include<iostream>
+#include <string>
+#include <cassert>
+
+class GLProgram
+{
+public:
+    GLProgram();
+    void attach(GLuint shaderId);
+    void detach(GLuint shaderId);
+    void getAttachedShaders(GLsizei maxCnt, GLsizei &count, GLuint* shaders);
+    bool isDeleted();
+    bool isLinked();
+    bool isValidate();
+    GLint getLogLength();
+    GLint getAttachedShadersNumber();
+    GLint getActiveAtomicCounterBuffers();
+    GLint getActiveAttributesNumber();
+    GLint getActiveAttributesMaxLength();
+    GLint getActiveUniformNumber();
+    GLint getActiveUniformMaxLength();
+    GLint getProgramBinaryLength();
+    void getComputeWorkGroupSize(GLint* param);
+    GLint getTransformFeedbackBufferMode();
+    GLint getTransformFeedbackVaryingVariableNumber();
+    GLint getTransformFeedbackVaryingVariableMaxLength();
+    GLint getGeometryVerticesOutputNumber();
+    GLint getGeometryInputNumber();
+    GLint getGeometryOutputNumber();
+    void getLog(std::string &log);
+    bool isProgram();
+    bool link();
+    void use();
+    void setProgramId(GLuint programId);
+    GLuint getProgramId();
+    //glGetActiveAttrib
+    //glGetProgramInterfaceiv
+    //glGetProgramResourceiv
+  
+private:
+    GLuint m_id;
+};
+
